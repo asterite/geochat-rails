93e19d0278090b43608962a66f4ef6eaa0376268
Ary Borenszweig <ary@esperanto.org.ar>
2011-01-30 15:02:11 -0300

    Fixed migration

--- db/migrate/20110130174707_initialize_group_login_downcase.rb
+++ db/migrate/20110130174707_initialize_group_login_downcase.rb
@@ -2,6 +2,7 @@ class InitializeGroupLoginDowncase < ActiveRecord::Migration
   def self.up
     Group.all.each do |group|
       group.alias_downcase = group.alias
+      group.save!
     end
   end
 
