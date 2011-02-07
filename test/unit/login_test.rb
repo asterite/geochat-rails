62f6445b577b322d761bab7533461a18293d5a8d
Ary Borenszweig <ary@esperanto.org.ar>
2011-02-06 23:19:53 -0300

    Shortened some tests

--- test/unit/login_test.rb
+++ test/unit/login_test.rb
@@ -4,41 +4,41 @@ require 'unit/pipeline_test'
 
 class LoginTest < PipelineTest
   test "login" do
-    send_message "sms://1", ".name User1"
-    send_message "sms://1", "#bye"
+    send_message 1, ".name User1"
+    send_message 1, "#bye"
 
-    send_message "sms://1", "I am User1 MockPassword"
+    send_message 1, "I am User1 MockPassword"
     assert_user_is_logged_in "sms://1", "User1"
-    assert_messages_sent_to "sms://1", "Hello User1. When you want to remove this device send: bye"
+    assert_messages_sent_to 1, "Hello User1. When you want to remove this device send: bye"
   end
 
   test "login wrong username" do
-    send_message "sms://1", ".name User1"
-    send_message "sms://1", "#bye"
+    send_message 1, ".name User1"
+    send_message 1, "#bye"
 
-    send_message "sms://1", "I am User2 MockPassword"
+    send_message 1, "I am User2 MockPassword"
     assert_channel_does_not_exist "sms://1"
     assert_messages_sent_to "sms://1", "Invalid login"
   end
 
   test "login wrong password" do
-    send_message "sms://1", ".name User1"
-    send_message "sms://1", "#bye"
+    send_message 1, ".name User1"
+    send_message 1, "#bye"
 
-    send_message "sms://1", "I am User1 WrongPassword"
+    send_message 1, "I am User1 WrongPassword"
     assert_channel_does_not_exist "sms://1"
-    assert_messages_sent_to "sms://1", "Invalid login"
+    assert_messages_sent_to 1, "Invalid login"
   end
 
   test "login when already logged in with another user" do
-    send_message "sms://1", ".name User1"
-    send_message "sms://1", "#bye"
-    send_message "sms://1", ".name User2"
-    send_message "sms://1", "I am User2 MockPassword"
+    send_message 1, ".name User1"
+    send_message 1, "#bye"
+    send_message 1, ".name User2"
+    send_message 1, "I am User2 MockPassword"
 
-    send_message "sms://1", "I am User1 MockPassword"
+    send_message 1, "I am User1 MockPassword"
     assert_user_is_logged_in "sms://1", "User1"
-    assert_messages_sent_to "sms://1", "Hello User1. When you want to remove this device send: bye"
+    assert_messages_sent_to 1, "Hello User1. When you want to remove this device send: bye"
   end
 
 end
--- test/unit/logout_test.rb
+++ test/unit/logout_test.rb
@@ -4,17 +4,17 @@ require 'unit/pipeline_test'
 
 class LogoutTest < PipelineTest
   test "logout" do
-    send_message "sms://1", ".name John Doe"
-    send_message "sms://1", "#bye"
+    send_message 1, ".name John Doe"
+    send_message 1, "#bye"
 
     assert_channel_does_not_exist "sms://1"
-    assert_messages_sent_to "sms://1", "John Doe, this device has been removed from your account."
+    assert_messages_sent_to 1, "John Doe, this device has been removed from your account."
   end
 
   test "logout twice" do
-    send_message "sms://1", "name John Doe"
-    send_message "sms://1", "bye"
-    send_message "sms://1", "bye"
+    send_message 1, "name John Doe"
+    send_message 1, "bye"
+    send_message 1, "bye"
 
     assert_channel_does_not_exist "sms://1"
     assert_not_logged_in_message_sent_to "sms://1"
