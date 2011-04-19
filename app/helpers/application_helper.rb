b7c6643fd277f7a4bf655e2c5c4d006d18ec26b1
asterite <ary@esperanto.org.ar>
2011-04-19 13:28:54 +0800

    Some style

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def geochat_version
+    begin
+      @@geochat_version = File.read('VERSION').strip unless defined? @@geochat_version
+    rescue Errno::ENOENT
+      @@geochat_version = 'Development'
+    end
+    @@geochat_version
+  end
 end
